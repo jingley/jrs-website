99026fad2fd0ff305ddbd365f2750abf2adb532b
Jon Ross Ingley <jingley22@gmail.com>
2020-03-29 10:35:50 -0400

    further updating placeholder homepage text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing this to see how it updates
+          This is a placeholder home page for my first React App deployed on github pages. This will be my Resume Page!
         </p>
         <a
           className="App-link"
