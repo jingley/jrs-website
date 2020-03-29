2acc29af9dd776f2e5adf67214c2c81f8cc2d80f
Jon Ross Ingley <jingley22@gmail.com>
2020-03-29 10:24:00 -0400

    Updates

---
+++ precache-manifest.80461b313494da91ea8d2916069600b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21601cc26aeefc1c8e13af4f98a7f6af",
+    "url": "/jrs-website/index.html"
+  },
+  {
+    "revision": "e141f8bc289518886176",
+    "url": "/jrs-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10c0a04d7406925d278c",
+    "url": "/jrs-website/static/js/2.479b475e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jrs-website/static/js/2.479b475e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e141f8bc289518886176",
+    "url": "/jrs-website/static/js/main.45035d05.chunk.js"
+  },
+  {
+    "revision": "1e7d477c7c079f6fca56",
+    "url": "/jrs-website/static/js/runtime-main.3cbce1a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jrs-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
