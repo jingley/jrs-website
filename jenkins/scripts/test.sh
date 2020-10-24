ae0bbeed76276689310383dd0e90d739dc23f3e7
Jon Ross Ingley <jingley22@gmail.com>
2020-10-23 20:13:28 -0400

    first was causing issues, but I need to find the proper location of shell

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
