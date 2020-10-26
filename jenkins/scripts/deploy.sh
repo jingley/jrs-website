39e3f282052c1c9801c1feb7ed33ef128485686d
Jon Ross Ingley <jingley22@gmail.com>
2020-10-26 16:28:36 -0400

    didnt work in the deploy.sh file, so Im moving it directly into the pipeline file.

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,5 @@
 echo 'The following commands set up credentials then builds the react app in production'
 echo 'then deploys it to gh-pages as described in the package.json'
 set -x
-git config --global user.email 'ci@jenkins.com' && git config --global user.name 'ci-auto'
-git config --global credential.helper '/bin/bash ${WORKSPACE}/ops/credentials-helper.sh'
-gh-pages --dotfiles --message '[skip ci] Updates' --dist docs/build/html
 npm run deploy
 set +x
