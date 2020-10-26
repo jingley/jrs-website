55cd79b73f945dc20301bb7be360bdf3cc5c05c0
Jon Ross Ingley <jingley22@gmail.com>
2020-10-26 16:30:15 -0400

    added an echo in deploy.sh because I am worried the checkout stage isnt using the right files

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,6 @@
 echo 'The following commands set up credentials then builds the react app in production'
 echo 'then deploys it to gh-pages as described in the package.json'
 set -x
+echo 'is the checkout stage actually pulling to correct stuff?'
 npm run deploy
 set +x
