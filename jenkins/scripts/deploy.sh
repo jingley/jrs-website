b341fbd28d0b482b7d407c4514c9f13b4301d7ff
Jon Ross Ingley <jingley22@gmail.com>
2020-10-26 15:24:55 -0400

    trying still to get gh-pages to work

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,3 @@
-echo ''
-set -x
-npm install -g --silent gh-pages@2.2.0
-git config --global user.email 'ci@jenkins.com' && git config --global user.name 'ci-auto'
-git config --global credential.helper '/bin/bash ${WORKSPACE}/ops/credentials-helper.sh'
-gh-pages --dotfiles --message '[skip ci] Updates' --dist docs/build/html
-set +x
-
 echo 'The following command builds the react app in production'
 echo 'then deploys it to gh-pages as described in the package.json'
 set -x
